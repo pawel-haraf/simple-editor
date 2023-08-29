7b8cada733bb8f32c8e0e362d66e82970ecc2d62
Pawel Haraf <haraf.p@gmail.com>
2023-08-29 22:17:18 +0200

    configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				wtf: '#557788',
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
